df6dd45def2fa5b589044b348f21cc3eabf9d1ee
kaushlakers <kaush.lakers@gmail.com>
2014-10-27 04:07:40 -0400

    Added work and projects

--- js/controller.js
+++ js/controller.js
@@ -9,11 +9,21 @@ phonecatApp.controller('ViewCtrl', function ($scope) {
     //{text:"Contact", url:"Views/contact.html"}
     ];
   $scope.currentMenu = $scope.menu[0];
-
+  $scope.backstretch = {flag:false,text:"You need a backstretch!"};
   $scope.setMenuItem = function(menu){
     $scope.currentMenu = menu;
   }
   $scope.setActiveClass = function(menu){
     return ($scope.currentMenu == menu)?"active":""
   }
+  $scope.setBackstretch = function(){
+    $scope.backstretch = {flag:true, text:"I warned you!"}
+    $.backstretch("images/bg-compressed.jpg");
+    $(".main-container").css("opacity",0.92);
+    $(".main-container").css("-khtml-opacity",0.92);
+    $(".main-container").css("-moz-opacity",0.92);
+    //$("#bs").prop('disabled', true);
+
+  };
+
 })
